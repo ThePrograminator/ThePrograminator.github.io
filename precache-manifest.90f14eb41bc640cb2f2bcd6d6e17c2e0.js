d0fc9f26cc9ee7304df485f48c7b13a3f5d063b6
ThePrograminator <christian_skyder@hotmail.com>
2021-04-05 22:29:43 +0200

    Updates

---
+++ precache-manifest.90f14eb41bc640cb2f2bcd6d6e17c2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c8d5185e09dacdc68c3db69bc6c0c5",
+    "url": "/ThePrograminator.github.io/index.html"
+  },
+  {
+    "revision": "461216af6888d19a66c2",
+    "url": "/ThePrograminator.github.io/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0af4997c40c4a4a3cece",
+    "url": "/ThePrograminator.github.io/static/css/main.edf2ea29.chunk.css"
+  },
+  {
+    "revision": "461216af6888d19a66c2",
+    "url": "/ThePrograminator.github.io/static/js/2.27717968.chunk.js"
+  },
+  {
+    "revision": "27f1c73f1dc921bc79aa41830d4cd146",
+    "url": "/ThePrograminator.github.io/static/js/2.27717968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af4997c40c4a4a3cece",
+    "url": "/ThePrograminator.github.io/static/js/main.74abcc48.chunk.js"
+  },
+  {
+    "revision": "c31146b039ccabd2bf56",
+    "url": "/ThePrograminator.github.io/static/js/runtime-main.006c3c82.js"
+  }
+]);
